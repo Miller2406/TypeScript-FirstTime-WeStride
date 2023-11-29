ff03f3e5fbad97ebae5ed80e789614fda0d727e8
Miller2406 <140726735+Miller2406@users.noreply.github.com>
2023-11-29 18:07:01 +0700

    update

--- welcomeToTS/annotations.ts
+++ welcomeToTS/annotations.ts
@@ -9,9 +9,8 @@ function fn(a: number, b: number): number {
 };
 console.log(fn(1, 2))
 
-// generic
+// generic 
 function fn2<A, B>(x: A, y: B) {
     return [x, y];
 };
-
 console.log(fn2<number, string>(5, '2'))
